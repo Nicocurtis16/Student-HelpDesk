c66414b51c8bada795eec314446dda25f1b98080
Nicocurtis16 <nicholaskyereboah@gmail.com>
2024-08-13 05:38:07 +0000

    have the red border arounf the history

--- src/app/Master-Dashboard/Layout/student/student.component.ts
+++ src/app/Master-Dashboard/Layout/student/student.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
-  styleUrl: './student.component.css'
+  styleUrls: ['./student.component.css']
 })
 export class StudentComponent {
 
+  editUser() {
+    // Your logic for editing a user
+    console.log('Edit User button clicked');
+  }
+
+  deleteUser() {
+    // Your logic for deleting a user
+    console.log('Delete User button clicked');
+  }
+  handleNextClick() {
+    // Your logic for handling the button click
+    console.log('Next button clicked');
+  }
+
+  handleButtonClick() {
+    // Your logic for handling button click
+    console.log('Button clicked');
+  }
+  
 }
+
